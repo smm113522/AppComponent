963678074648ca5de62d9ad4cf099c3fd2dfe690
shimengmeng <yanbin@yuanshunwl.com>
2019-04-28 10:19:14 +0800

    最代码

---
+++ module_shop/src/main/java/com/tool/shop/ShopFragment.java
@@ -0,0 +1,15 @@
+package com.tool.shop;
+
+import com.tool.style.base.BaseFragment;
+
+public class ShopFragment extends BaseFragment {
+
+
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_shop;
+    }
+
+
+}

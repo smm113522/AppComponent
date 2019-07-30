0dec76d17ba47285234b87ba4cefcdf08410038a
shimengmeng <yanbin@yuanshunwl.com>
2019-07-30 14:15:31 +0800

    初始化项目

--- app/src/main/java/com/kotlin/app/MainActivity.kt
+++ app/src/main/java/com/kotlin/app/MainActivity.kt
@@ -2,11 +2,17 @@ package com.kotlin.app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.alibaba.android.arouter.launcher.ARouter
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        val uri = intent.data
+        val uri = "/test/activity"
+        ARouter.getInstance().build(uri).navigation()
+        finish()
     }
 }
--- module/module_main/src/main/java/com/kotlin/MainActivity.kt
+++ module/module_main/src/main/java/com/kotlin/MainActivity.kt
@@ -2,8 +2,10 @@ package com.kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.alibaba.android.arouter.facade.annotation.Route
 import com.btmv.module_main.R
 
+@Route(path = "/test/activity")
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {

5740e0386814df3b4ec46047363d6bafbfb48e8f
smm <yanbin@yuanshunwl.com>
2019-08-18 22:50:39 +0800

    文档更新。。学习目的和收集

---
+++ module/module_main/src/main/java/com/kotlin/ImageActivity.kt
@@ -0,0 +1,20 @@
+package com.kotlin
+
+import com.code.base.BaseActivity
+
+class ImageActivity : BaseActivity() {
+
+
+    override fun getLayout(): Int {
+        return 0
+    }
+
+    override fun getViewDataLayout(): Int {
+        return 0
+    }
+
+    override fun initView() {
+
+    }
+
+}

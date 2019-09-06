0dfd61a00b1871fbb792085a40bc21dbe785172c
shimengmeng <yanbin@yuanshunwl.com>
2019-09-06 17:16:50 +0800

    安装不了问题修复

--- app/src/main/java/com/kotlin/app/App.kt
+++ app/src/main/java/com/kotlin/app/App.kt
@@ -6,12 +6,35 @@ import android.content.Context
 import android.os.Build
 import android.support.multidex.MultiDex
 import com.alibaba.android.arouter.launcher.ARouter
+import com.argusapm.android.api.Client
 import com.code.BuildConfig
 import com.code.base.BaseApp
 import com.tencent.bugly.beta.Beta
+import com.argusapm.android.api.Client.startWork
+import com.argusapm.android.core.Config
+import com.argusapm.android.network.upload.CollectDataSyncUpload
+import com.argusapm.android.network.cloudrule.RuleSyncRequest
+import com.argusapm.android.core.Config.ConfigBuilder
+import com.qihoo360.loader2.PluginStatusController.setAppContext
+import com.tencent.bugly.crashreport.CrashReport.setAppVersion
+
+
 
 class App : BaseApp() {
 
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+
+        val builder = Config.ConfigBuilder()
+                .setAppContext(this)
+                .setAppName("apm_demo")
+                .setRuleRequest(RuleSyncRequest())
+                .setUpload(CollectDataSyncUpload())
+                .setAppVersion("0.0.1")
+                .setApmid("qh1uzr0mp2zi")
+        Client.attach(builder.build())
+        Client.startWork()
+    }
 
     override fun onCreate() {
         super.onCreate()

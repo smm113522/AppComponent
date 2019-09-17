9949decfe37b7aef1c6a2cbc3b1606d9c4d359ec
shimengmeng <yanbin@yuanshunwl.com>
2019-09-17 10:04:14 +0800

    所有代码。。

---
+++ common/lib_code/src/main/java/com/code/FragmentActivity.java
@@ -0,0 +1,17 @@
+package com.code;
+
+import com.code.base.BaseActivity;
+
+public class FragmentActivity extends BaseActivity {
+
+
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}

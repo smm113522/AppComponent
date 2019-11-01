679e3e74bd4b3f86bfd2d730ebf55fbfdf5456be
shimengmeng <yanbin@yuanshunwl.com>
2019-11-01 08:53:52 +0800

    最新代码

--- app/src/main/java/com/kotlin/replugin/MainActivity.java
+++ app/src/main/java/com/kotlin/replugin/MainActivity.java
@@ -2,12 +2,7 @@ package com.kotlin.replugin;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-
-import com.qihoo360.replugin.RePlugin;
 
 
 public class MainActivity extends Activity {

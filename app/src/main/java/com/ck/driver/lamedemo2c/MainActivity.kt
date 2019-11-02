0fe15b46ec29b2ff28f1977ca37cba0f01c87f24
shimengmeng <yanbin@yuanshunwl.com>
2019-11-02 21:36:29 +0800

    最新代码，，，选择文件 。。。

--- app/src/main/java/com/ck/driver/lamedemo2c/MainActivity.kt
+++ app/src/main/java/com/ck/driver/lamedemo2c/MainActivity.kt
@@ -1,17 +1,86 @@
 package com.ck.driver.lamedemo2c
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.thl.filechooser.FileChooser
+import com.thl.filechooser.FileInfo
 import kotlinx.android.synthetic.main.activity_main.*
+import androidx.annotation.NonNull
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    var path_source = ""// 获取 file 的路径
+    var mp3_path_source = "" // 文件mp3的路径
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestPermissins(object : PermissionUtils.OnPermissionListener {
+            override fun onPermissionGranted() {
+
+            }
+
+            override fun onPermissionDenied(deniedPermissions: Array<String>) {
+                Toast.makeText(this@MainActivity, "未获取到存储权限", Toast.LENGTH_SHORT).show()
+            }
+        })
+
         // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+        sample_text.text = stringFromJNI()// 测试native 里面的方法
+        /**
+         * 获取文件
+         */
+        bt_file.setOnClickListener {
+
+            requestPermissins(object : PermissionUtils.OnPermissionListener {
+                override fun onPermissionGranted() {
+
+                    val fileChooser =
+                        FileChooser(this@MainActivity, object : FileChooser.FileChoosenListener {
+                            override fun onFileChoosen(filePath: String) {
+                                path_source = filePath
+                                tv_file_path.text = path_source
+                            }
+                        })
+
+                    fileChooser.setBackIconRes(R.drawable.btn_back)
+                    fileChooser.setTitle("选择文件路径")
+                    fileChooser.setDoneText("确定")
+                    fileChooser.setThemeColor(R.color.colorPrimary)
+
+                    fileChooser.setChooseType(FileInfo.FILE_TYPE_FILE)
+                    fileChooser.showFile(true)  //是否显示文件
+                    fileChooser.open()
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: Array<String>) {
+                    Toast.makeText(this@MainActivity, "未获取到存储权限", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+        /**
+         * 转换文件
+         */
+        bt_encoder.setOnClickListener {
+
+
+        }
+        /**
+         * 播放mp3
+         */
+        bt_player.setOnClickListener {
+
+        }
+
     }
 
     /**
@@ -26,5 +95,26 @@ class MainActivity : AppCompatActivity() {
         init {
             System.loadLibrary("native-lib")
         }
+    }
+
+    private fun requestPermissins(mOnPermissionListener: PermissionUtils.OnPermissionListener) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            mOnPermissionListener.onPermissionGranted()
+            return
+        }
+        val permissions = arrayOf(
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE"
+        )
+        PermissionUtils.requestPermissions(this, 0, permissions, mOnPermissionListener)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        PermissionUtils.onRequestPermissionsResult(this, requestCode, permissions, grantResults)
     }
 }

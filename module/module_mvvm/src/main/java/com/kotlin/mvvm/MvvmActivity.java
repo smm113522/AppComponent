ebde584b63e80589d2c7afea4ee56e410bfc6120
乐观开发者 <908085643@qq.com>
2020-05-20 14:57:22 +0800

    1,发现问题。。 2，路由没法用了。。和原来的风格就不一样了。

--- module/module_mvvm/src/main/java/com/kotlin/mvvm/MvvmActivity.java
+++ module/module_mvvm/src/main/java/com/kotlin/mvvm/MvvmActivity.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-import com.alibaba.android.arouter.facade.annotation.Route;
 import com.kotlin.mvvm.databinding.ActivityMvvmBinding;
 
-@Route(path = "/mvvm/activity")
+//@Route(path = "/mvvm/activity")
+
 public class MvvmActivity extends AppCompatActivity {
 
     private ActivityMvvmBinding mvvmBinding;
@@ -16,9 +16,8 @@ public class MvvmActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mvvmBinding = DataBindingUtil.setContentView(MvvmActivity.this,R.layout.activity_mvvm);
-        setContentView(mvvmBinding.getRoot());
-
+        mvvmBinding = DataBindingUtil.setContentView(MvvmActivity.this, R.layout.activity_mvvm);
+//        setContentView(mvvmBinding.getRoot());
 
 
     }

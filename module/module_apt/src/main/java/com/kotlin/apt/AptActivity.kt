66baaa8b1632f8874fef8c56c2f66efc11dceacb
乐观开发者 <908085643@qq.com>
2020-05-20 13:43:48 +0800

    1,重构了一下。添加了lib_common 代码                   2，测试了一下独立打包能力。                   3,还有包名和其它配置还不全。。。。

--- module/module_apt/src/main/java/com/kotlin/apt/AptActivity.kt
+++ module/module_apt/src/main/java/com/kotlin/apt/AptActivity.kt
@@ -2,13 +2,11 @@ package com.kotlin.apt
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
-import com.alibaba.android.arouter.facade.annotation.Route
-import com.code.utils.NavigationUtil
-import com.code.utils.RouterPath
+//import com.alibaba.android.arouter.facade.annotation.Route
+//import com.code.utils.RouterPath
 
 
-@Route(path = RouterPath.path_apt_activity)
+//@Route(path = RouterPath.path_apt_activity)
 class AptActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +16,9 @@ class AptActivity : AppCompatActivity(){
 
 //    @MyRoute(paths = "")
 //    @MyRoute(paths = RouterPath.path_pdf_activity)
-    fun ButtonCS(view : View){
-        NavigationUtil.toActivity(RouterPath.path_pdf_activity)
-    }
+//    fun ButtonCS(view : View){
+//        NavigationUtil.toActivity(RouterPath.path_pdf_activity)
+//    }
 
 
 }

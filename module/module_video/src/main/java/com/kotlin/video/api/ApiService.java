63334566f9e3bd8be07d917d9ce1c7ef584ffdba
乐观开发者 <908085643@qq.com>
2020-05-26 08:26:14 +0800

    网络获取接口 添加。

---
+++ module/module_video/src/main/java/com/kotlin/video/api/ApiService.java
@@ -0,0 +1,36 @@
+package com.kotlin.video.api;
+
+
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+
+ * 文件名:    ApiService
+ * 创建时间:  2019-03-27 on 14:55
+ * 描述:     TODO 声明接口
+ *
+ */
+
+public interface ApiService {
+
+    String BASE_URL = "https://news-at.zhihu.com/api/4/";
+
+    String JUE_JIN_BASE_URL = "http://timeline-merger-ms.juejin.im/v1/";
+
+    /**
+     * 测试接口
+     */
+    @GET("news/latest")
+    Observable<String> news();
+
+    /**
+     * 掘金接口
+     */
+    @GET("get_entry_by_timeline")
+    Observable<String> jueJin(@Query("category") String category, @Query("limit") String limit,
+                                  @Query("src") String src);
+
+}

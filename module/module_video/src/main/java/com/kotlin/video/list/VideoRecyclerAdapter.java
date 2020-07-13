5c74a22dcad1a293857225ef5fbd1423aa32336a
乐观开发者 <908085643@qq.com>
2020-07-13 09:51:35 +0800

    都有报错问题。。和list 播放 区分开来。。

--- module/module_video/src/main/java/com/kotlin/video/list/VideoRecyclerAdapter.java
+++ module/module_video/src/main/java/com/kotlin/video/list/VideoRecyclerAdapter.java
@@ -54,7 +54,7 @@ public class VideoRecyclerAdapter extends RecyclerView.Adapter<VideoRecyclerAdap
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_video, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_video_list, parent, false);
         return new ViewHolder(v);
     }
 

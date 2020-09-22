d47f4c660659ec978b7eb63683417d07a952f509
乐观开发者 <908085643@qq.com>
2020-09-22 10:55:24 +0800

    统一了一下api

--- module/module_ui/src/main/java/com/kotlin/module_ui/DemoUiActivity.kt
+++ module/module_ui/src/main/java/com/kotlin/module_ui/DemoUiActivity.kt
@@ -1,13 +1,19 @@
 package com.kotlin.module_ui
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.shuyu.gsyvideoplayer.GSYBaseActivityDetail
+import com.shuyu.gsyvideoplayer.builder.GSYVideoOptionBuilder
+import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer
 
-class DemoUiActivity: AppCompatActivity(R.layout.activity_demo_ui) {
+class DemoUiActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo_ui)
 
     }
+
 }

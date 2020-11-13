01d4cb6ad30ebaf85ce60d9067d147e86d0a3e68
乐观开发者 <908085643@qq.com>
2020-11-13 17:58:16 +0800

    组件化。。

---
+++ demo/demo-skin/src/main/java/com/kotlin/skin/SkinActivity.kt
@@ -0,0 +1,13 @@
+package com.kotlin.skin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SkinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
---
+++ demo/demo-skin/src/main/java/com/kotlin/skin/SkinApp.kt
@@ -0,0 +1,13 @@
+package com.kotlin.skin
+
+import android.app.Application
+
+class SkinApp : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
